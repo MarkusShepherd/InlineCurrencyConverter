600f9ccf28c5b66d0681ba4e65f26ec247f0052d
Markus Schepke <mschepke@yahoo-inc.com>
2016-02-14 18:26:31 +0800

    added exchange rate API call and remove need to mark up currency by hand

--- inlinecurrency.js
+++ inlinecurrency.js
@@ -1,11 +1,47 @@
 var rates = {};
-var regex = /(^|\W)([A-Za-z]{3})(\d+(\.\d{2})?)($|\W)/im
+var valueRegex = /(^|\W)([A-Za-z]{3})(\d+(\.\d{2})?)($|\W)/im;
+var classRegex = /([A-Za-z]{3}\d+(\.\d{2})?)/igm;
+var currencyClass = "currency";
+var elementQuery = "." + currencyClass;
+var targetCurr = "EUR";
+
+var markTextWithClass = function(regex, classStr) {
+  var textNodes = getTextNodesIn($("body"));
+  textNodes.replaceWith(function() {
+    var text = $(this)[0].textContent;
+    console.log(text);
+    return text.replace(regex, '<span class="' + classStr + '">$1</span>');
+  });
+};
+
+var getRates = function(targetCurrency, updateElements) {
+  targetCurrency = targetCurrency.toUpperCase();
+  $.getJSON( "http://api.fixer.io/latest?base=" + targetCurrency, function(data) {
+    var r;
+    if (targetCurrency in rates)
+      r = rates[targetCurrency];
+    else {
+      r = {};
+      rates[targetCurrency] = r;
+    }
+
+    $.extend(r, data.rates);
+    $.each(data.rates, function(curr, val ) {
+      if (!(curr in rates))
+        rates[curr] = {};
+      rates[curr][targetCurrency] = 1/val;
+    });
+
+    if (updateElements)
+      $(elementQuery).each(convertElement(targetCurrency));
+  });
+};
 
 var convertElement = function(targetCurrency) {
   targetCurrency = targetCurrency.toUpperCase();
   return function(index) {
-    var text = $( this ).text();
-    var matches = regex.exec(text);
+    var text = $(this).text();
+    var matches = valueRegex.exec(text);
     var baseCurrency = matches[2].toUpperCase();
     var value = parseFloat(matches[3]);
 
@@ -16,13 +52,23 @@ var convertElement = function(targetCurrency) {
 
     if (baseCurrency in rates && targetCurrency in rates[baseCurrency]) 
       rate = rates[baseCurrency][targetCurrency];
-    else {
-      // TODO make XHR to fill rates
-    }
+    else
+      return;
 
     var valueConverted = value * rate;
 
-    text = baseCurrency + value + ' (' + targetCurrency + valueConverted + ')';
-    $( this ).html(text);
+    text = baseCurrency + value.toFixed(2) + ' (' + targetCurrency + valueConverted.toFixed(2) + ')';
+    $(this).html(text);
   };
 };
+
+var getTextNodesIn = function(el) {
+  return $(el).find(":not(iframe)").addBack().contents().filter(function() {
+    return this.nodeType === 3;
+  });
+};
+
+$(function(){
+  markTextWithClass(classRegex, currencyClass);
+  getRates(targetCurr, true);
+});
